8ef73802fda374b208cef14f493a53f63c61ffec
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-22 11:48:26 +0200

    Update-Added Login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,9 +1,34 @@
 <template>
-  <p>Login</p>
+  <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
+    <input type="email" placeholder="Email" v-model="email">
+    <input type="password" placeholder="Password"  v-model="password">
+    <div v-if="error" class="error">{{ error }}</div>
+    <button >Login</button>
+  </form>
 </template>
 
 <script>
+import useLogin from '@/composables/useLogin'
+import { ref } from 'vue'
+
 export default {
+  setup(){
+    const email = ref('')
+    const password = ref('')
+
+    const { error, login} = useLogin()
+
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value)
+      if(!error.value){
+        console.log('User logged in')
+      }
+    }
+
+    return {password, email, handleSubmit, error}
+  }
+
 
 }
 </script>
