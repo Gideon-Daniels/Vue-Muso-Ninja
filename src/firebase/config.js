46900717e56bd497aeaa9a00c020e7163d63d511
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-22 10:37:14 +0200

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeH14pJLsWn8U-whiGtP-_QuN8j3z-OHE",
+    authDomain: "muso-ninjas-eaf54.firebaseapp.com",
+    projectId: "muso-ninjas-eaf54",
+    storageBucket: "muso-ninjas-eaf54.appspot.com",
+    messagingSenderId: "34626747002",
+    appId: "1:34626747002:web:b11a5bb2670b3e25d519c3"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const proejectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, proejectAuth, timestamp}
